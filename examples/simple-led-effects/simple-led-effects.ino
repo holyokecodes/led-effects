45e0711cd5c12e2ff751a44e032eabf06ce77041
Andrew Pasquale <andrew@elytra.net>
2018-12-13 11:40:25 -0500

    Update example code

--- examples/simple-led-effects/simple-led-effects.ino
+++ examples/simple-led-effects/simple-led-effects.ino
@@ -9,26 +9,26 @@ NeoPatterns leds(NUMPIXELS, ledPin, done);
 
 // Define some colors with RGB values
 uint32_t red = leds.Color(255, 0, 0);
-uint32_t green = leds.Color(0, 255, 0);
+uint32_t blue = leds.Color(0, 0, 255);
 
 // Initialize everything and prepare to start
 void setup()
 { 
   leds.begin();
-  leds.ColorSet(green);
-  leds.Fade(red, green, 50, 100); // color1, color2, steps, interval
-  leds.RainbowCycle(20);          // interval
-  leds.Scanner(red, 50);          // color, interval
-  leds.TheaterChase(red, green, 150);
 }
 
 // Main loop
 void loop()
 {
+  // Fade from Color1 to Color2
+  leds.ActivePattern = FADE;
+  leds.Color1 = red;
+  leds.Color2 = blue;
+  leds.Interval = 100;
+  leds.TotalSteps = 100;
   leds.Update(); 
 }
 
 void done() {
   leds.Reverse();
 }
-
